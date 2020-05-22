d33fb779c1daf3080f342715c8f866f0f86df402
Andrew Querol <aquerol@percipia.com>
2020-05-21 21:41:59 -0500

    Fix not using the supplied password in Dial

--- inbound.go
+++ inbound.go
@@ -17,7 +17,7 @@ func Dial(address, password string) (*Conn, error) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	response, err := connection.SendCommand(ctx, command.Auth{Password: "ClueCon"})
+	response, err := connection.SendCommand(ctx, command.Auth{Password: password})
 	if err != nil {
 		return nil, err
 	}
