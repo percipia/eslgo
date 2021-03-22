775d06ab5678b8581fd75788ef6991304258a4c6
Andrew Querol <aquerol@percipia.com>
2021-03-22 13:51:38 -0500

    And in better options and logger support.
    This is to allow more configurability and user freedom when it comes to using the library. It still remains backwards compatible with the option-less API. See #7

--- inbound.go
+++ inbound.go
@@ -14,34 +14,54 @@ import (
 	"context"
 	"fmt"
 	"github.com/percipia/eslgo/command"
-	"log"
 	"net"
 )
 
+// InboundOptions - Used to dial a new inbound ESL connection to FreeSWITCH
+type InboundOptions struct {
+	Options             // Generic common options to both Inbound and Outbound Conn
+	Address      string // The address to pass into dial, normally 127.0.0.1:8084
+	Network      string // The network type to use, should always be tcp, tcp4, tcp6.
+	Password     string // The password used to authenticate with FreeSWITCH. Usually ClueCon
+	OnDisconnect func() // An optional function to be called with the inbound connection gets disconnected
+}
+
+// Dial - Connects to FreeSWITCH ESL at the provided address and authenticates with the provided password. onDisconnect is called when the connection is closed either by us, FreeSWITCH, or network error
 func Dial(address, password string, onDisconnect func()) (*Conn, error) {
-	c, err := net.Dial("tcp", address)
+	return InboundOptions{
+		Options:      Options{Logger: NormalLogger{}},
+		Address:      address,
+		Network:      "tcp",
+		Password:     password,
+		OnDisconnect: onDisconnect,
+	}.Dial()
+}
+
+// Dial - Connections to FreeSWITCH ESL with the provided options. Returns the connection and any errors encountered
+func (opts InboundOptions) Dial() (*Conn, error) {
+	c, err := net.Dial(opts.Network, opts.Address)
 	if err != nil {
 		return nil, err
 	}
-	connection := newConnection(c, false)
+	connection := newConnection(c, false, opts.Options)
 
 	// First auth
 	<-connection.responseChannels[TypeAuthRequest]
-	err = connection.doAuth(connection.runningContext, command.Auth{Password: password})
+	err = connection.doAuth(connection.runningContext, command.Auth{Password: opts.Password})
 	if err != nil {
 		// Try to gracefully disconnect, we have the wrong password.
 		connection.ExitAndClose()
-		if onDisconnect != nil {
-			go onDisconnect()
+		if opts.OnDisconnect != nil {
+			go opts.OnDisconnect()
 		}
 		return nil, err
 	} else {
-		log.Printf("Sucessfully authenticated %s\n", connection.conn.RemoteAddr())
+		connection.logger.Info("Successfully authenticated %s\n", connection.conn.RemoteAddr())
 	}
 
 	// Inbound only handlers
-	go connection.authLoop(command.Auth{Password: password})
-	go connection.disconnectLoop(onDisconnect)
+	go connection.authLoop(command.Auth{Password: opts.Password})
+	go connection.disconnectLoop(opts.OnDisconnect)
 
 	return connection, nil
 }
@@ -65,12 +85,12 @@ func (c *Conn) authLoop(auth command.Auth) {
 		case <-c.responseChannels[TypeAuthRequest]:
 			err := c.doAuth(c.runningContext, auth)
 			if err != nil {
-				log.Printf("Failed to auth %e\n", err)
+				c.logger.Warn("Failed to auth %e\n", err)
 				// Close the connection, we have the wrong password
 				c.ExitAndClose()
 				return
 			} else {
-				log.Printf("Sucessfully authenticated %s\n", c.conn.RemoteAddr())
+				c.logger.Info("Successfully authenticated %s\n", c.conn.RemoteAddr())
 			}
 		case <-c.runningContext.Done():
 			return
--- outbound.go
+++ outbound.go
@@ -14,45 +14,72 @@ import (
 	"context"
 	"errors"
 	"github.com/percipia/eslgo/command"
-	"log"
 	"net"
 	"time"
 )
 
 type OutboundHandler func(ctx context.Context, conn *Conn, connectResponse *RawResponse)
 
+// OutboundOptions - Used to open a new listener for outbound ESL connections from FreeSWITCH
+type OutboundOptions struct {
+	Options                       // Generic common options to both Inbound and Outbound Conn
+	Address         string        // The address to bind to for listening for inbound FreeSWITCH connections
+	Network         string        // The network type to listen on, should be tcp, tcp4, or tcp6
+	ConnectTimeout  time.Duration // How long should we wait for FreeSWITCH to respond to our "connect" command. 5 seconds is a sane default.
+	ExitTimeout     time.Duration // How long should we wait for FreeSWITCH to respond to our "exit" command. 5 seconds is a sane default.
+	ConnectionDelay time.Duration // How long should we wait after connection to start sending commands. 25ms is the recommended default otherwise we can close the connection before FreeSWITCH finishes starting it on their end. https://github.com/signalwire/freeswitch/pull/636
+}
+
 /*
  * TODO: Review if we should have a rate limiting facility to prevent DoS attacks
  * For our use it should be fine since we only want to listen on localhost
  */
+// ListenAndServe - Open a new listener for outbound ESL connections from FreeSWITCH on the specified address with the provided connection handler
 func ListenAndServe(address string, handler OutboundHandler) error {
-	listener, err := net.Listen("tcp", address)
+	return OutboundOptions{
+		Options:         Options{Logger: NormalLogger{}},
+		Address:         address,
+		Network:         "tcp",
+		ConnectTimeout:  5 * time.Second,
+		ExitTimeout:     5 * time.Second,
+		ConnectionDelay: 25 * time.Millisecond,
+	}.ListenAndServe(handler)
+}
+
+// ListenAndServe - Open a new listener for outbound ESL connections from FreeSWITCH with provided options and handle them with the specified handler
+func (opts OutboundOptions) ListenAndServe(handler OutboundHandler) error {
+	listener, err := net.Listen(opts.Network, opts.Address)
 	if err != nil {
 		return err
 	}
-	log.Printf("Listenting for new ESL connections on %s\n", listener.Addr().String())
+	if opts.Logger != nil {
+		opts.Logger.Info("Listening for new ESL connections on %s\n", listener.Addr().String())
+	}
 	for {
 		c, err := listener.Accept()
 		if err != nil {
 			break
 		}
+		conn := newConnection(c, true, opts.Options)
 
-		log.Printf("New outbound connection from %s\n", c.RemoteAddr().String())
-		conn := newConnection(c, true)
+		conn.logger.Info("New outbound connection from %s\n", c.RemoteAddr().String())
 		go conn.dummyLoop()
 		// Does not call the handler directly to ensure closing cleanly
-		go conn.outboundHandle(handler)
+		go conn.outboundHandle(handler, opts.ConnectionDelay, opts.ConnectTimeout, opts.ExitTimeout)
 	}
-	log.Println("Outbound server shutting down")
+
+	if opts.Logger != nil {
+		opts.Logger.Info("Outbound server shutting down")
+	}
 	return errors.New("connection closed")
 }
 
-func (c *Conn) outboundHandle(handler OutboundHandler) {
-	ctx, cancel := context.WithTimeout(c.runningContext, 5*time.Second)
+func (c *Conn) outboundHandle(handler OutboundHandler, connectionDelay, connectTimeout, exitTimeout time.Duration) {
+	ctx, cancel := context.WithTimeout(c.runningContext, connectTimeout)
 	response, err := c.SendCommand(ctx, command.Connect{})
 	cancel()
 	if err != nil {
-		log.Printf("Error connecting to %s error %s", c.conn.RemoteAddr().String(), err.Error())
+		c.logger.Warn("Error connecting to %s error %s", c.conn.RemoteAddr().String(), err.Error())
 		// Try closing cleanly first
 		c.Close() // Not ExitAndClose since this error connection is most likely from communication failure
 		return
@@ -61,10 +88,9 @@ func (c *Conn) outboundHandle(handler OutboundHandler) {
 	// XXX This is ugly, the issue with short lived async sockets on our end is if they complete too fast we can actually
 	// close the connection before FreeSWITCH is in a state to close the connection on their end. 25ms is an magic value
 	// found by testing to have no failures on my test system. I started at 1 second and reduced as far as I could go.
-	// TODO We should open a bug report on the FreeSWITCH GitHub at some point and remove this when fixed.
 	// TODO This actually may be fixed: https://github.com/signalwire/freeswitch/pull/636
-	time.Sleep(25 * time.Millisecond)
-	ctx, cancel = context.WithTimeout(c.runningContext, 5*time.Second)
+	time.Sleep(connectionDelay)
+	ctx, cancel = context.WithTimeout(c.runningContext, exitTimeout)
 	_, _ = c.SendCommand(ctx, command.Exit{})
 	cancel()
 	c.ExitAndClose()
@@ -73,10 +99,10 @@ func (c *Conn) outboundHandle(handler OutboundHandler) {
 func (c *Conn) dummyLoop() {
 	select {
 	case <-c.responseChannels[TypeDisconnect]:
-		log.Println("Disconnect outbound connection", c.conn.RemoteAddr())
+		c.logger.Info("Disconnect outbound connection", c.conn.RemoteAddr())
 		c.Close()
 	case <-c.responseChannels[TypeAuthRequest]:
-		log.Println("Ignoring auth request on outbound connection", c.conn.RemoteAddr())
+		c.logger.Debug("Ignoring auth request on outbound connection", c.conn.RemoteAddr())
 	case <-c.runningContext.Done():
 		return
 	}
